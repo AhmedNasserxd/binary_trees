d2be7fc750206d0555c39add04bdd57bd028423e
AhmedNasserxd <144343880+AhmedNasserxd@users.noreply.github.com>
2024-03-29 03:25:05 +0200

    Create 134-heap_to_sorted_array.c

---
+++ 134-heap_to_sorted_array.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * heap_to_sorted_array - Transforms a MB Heap to a sorted array
+ * @heap: a pointer to the node's root
+ * @size: the address to use
+ *
+ * Return: the created array
+ *         NULL on failure
+ */
+int *heap_to_sorted_array(heap_t *heap, size_t *size)
+{
+
+}
