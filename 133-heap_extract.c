d9c43c0b281d8b7c7982b99f798f4164d0c407e1
AhmedNasserxd <144343880+AhmedNasserxd@users.noreply.github.com>
2024-03-29 03:23:31 +0200

    Create 133-heap_extract.c

---
+++ 133-heap_extract.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * heap_extract - Obtains the node's root of a MB Heap
+ * @root: a double pointer to use
+ *
+ * Return: the value stored in the node's root
+ *         0 on failure
+ */
+int heap_extract(heap_t **root)
+{
+
+}
