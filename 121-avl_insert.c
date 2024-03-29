d910b4af7ae300c4e31b5034fa3cab3d0dfd407e
Ahmed Raafat Sayed <arsayedeg@gmail.com>
2024-03-29 05:04:32 +0200

    Update 121-avl_insert.c

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -1,49 +1,41 @@
 #include "binary_trees.h"
 
-int binary_tree_balance(const binary_tree_t *tree);
-size_t binary_tree_height(const binary_tree_t *tree);
+int balance(const binary_tree_t *tree);
+size_t height(const binary_tree_t *tree);
 avl_t *avl_insert_recursive(avl_t **tree, avl_t *parent,
 		avl_t **new, int value);
 avl_t *avl_insert(avl_t **tree, int value);
 
 /**
- * binary_tree_balance - to evaluate the balance factor of a binary tree
- * @tree: pointer to use
- *
- * Return: the balance factor
- *         0 when tree is NULL
- */
-int binary_tree_balance(const binary_tree_t *tree)
-{
-	int height_l, height_r;
-
-	if (!tree)
-		return (0);
-
-	height_l = tree->left ? (int)binary_tree_height(tree->left) : -1;
-	height_r = tree->right ? (int)binary_tree_height(tree->right) : -1;
-
-	return (height_l - height_r);
-}
-
-/**
- * binary_tree_height - to evaluate the height of a binary tree
+ * height - to evaluate the height of a binary tree
  * @tree: tree to use
  *
  * Return: height of the tree
  *         0 when tree is NULL
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t height(const binary_tree_t *tree)
 {
-	size_t height_l = 0;
-	size_t height_r = 0;
+	if (tree != NULL)
+	{
+		size_t l = 0, r = 0;
 
-	if (!tree)
-		return (0);
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		return ((l > r) ? l : r);
+	}
+	return (0);
+}
 
-	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-	return (height_l > height_r ? height_l : height_r);
+/**
+ * balance - to evaluate the balance factor of a binary tree
+ * @tree: pointer to use
+ *
+ * Return: the balance factor
+ *         0 when tree is NULL
+ */
+int balance(const binary_tree_t *tree)
+{
+	return (tree != NULL ? height(tree->left) - height(tree->right) : 0);
 }
 
 /**
