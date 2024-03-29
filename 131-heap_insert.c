e2baeb0a76a3c425036bc438e374d9fff896dd8c
AhmedNasserxd <144343880+AhmedNasserxd@users.noreply.github.com>
2024-03-29 05:13:06 +0200

    Create 131-heap_insert.c

---
+++ 131-heap_insert.c
@@ -0,0 +1,56 @@
+#include "binary_trees.h"
+
+/**
+ * heap_insert - to add a value in MB Heap
+ * @root: a double pointer to the node's root
+ * @value: the value to use
+ *
+ * Return: a pointer to the node
+ *         NULL on failure
+ */
+heap_t *heap_insert(heap_t **root, int value)
+{
+	heap_t *tree, *new, *flip;
+	int size, leaves, sub, bit, level, tmp;
+
+	if (!root)
+		return (NULL);
+	if (!(*root))
+		return (*root = binary_tree_node(NULL, value));
+	tree = *root;
+	size = binary_tree_size(tree);
+	leaves = size;
+	for (level = 0, sub = 1; leaves >= sub; sub *= 2, level++)
+		leaves -= sub;
+
+	for (bit = 1 << (level - 1); bit != 1; bit >>= 1)
+		tree = leaves & bit ? tree->right : tree->left;
+	new = binary_tree_node(tree, value);
+	leaves & 1 ? (tree->right = new) : (tree->left = new);
+
+	flip = new;
+	for (; flip->parent && (flip->n > flip->parent->n); flip = flip->parent)
+	{
+		tmp = flip->n;
+		flip->n = flip->parent->n;
+		flip->parent->n = tmp;
+		new = new->parent;
+	}
+
+	return (new);
+}
+
+/**
+ * binary_tree_size - to evaluate the size of a binary tree
+ * @tree: tree to use
+ *
+ * Return: size of the tree
+ *         0 if tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
